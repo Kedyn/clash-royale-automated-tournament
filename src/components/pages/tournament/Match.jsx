0df9140b07c6bc4fe78acc5fa0940e6fe941beda
Kedyn <mkedyn@gmail.com>
2019-12-05 23:29:47 -0800

    data generated bracket

--- src/components/pages/tournament/Match.jsx
+++ src/components/pages/tournament/Match.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import { Row, Col } from "react-bootstrap";
 
 export default class Match extends Component {
   render() {
+    const { data } = this.props;
     return (
       <React.Fragment>
         <div className="match">
-          <div className="player">Player 1</div>
-          <div className="player">Player 2</div>
+          <Row className="player">
+            <Col xs="8">{data.players[0].name}</Col>
+            <Col className="text-right">{data.players[0].score}</Col>
+          </Row>
+          <Row className="player">
+            <Col xs="8">{data.players[1].name}</Col>
+            <Col className="text-right">{data.players[1].score}</Col>
+          </Row>
         </div>
       </React.Fragment>
     );
--- src/components/pages/tournament/TournamentPage.jsx
+++ src/components/pages/tournament/TournamentPage.jsx
@@ -10,36 +10,78 @@ export default class TournamentPage extends Component {
 
     this.state = {
       tournamnet: {
-        players: [],
-        rounds: [{}]
+        players: ["Player 1", "Player 2", "Player 3", "Player 4"],
+        rounds: [
+          {
+            best: 1,
+            matches: [
+              {
+                players: [
+                  {
+                    name: "Player 1",
+                    score: 1
+                  },
+                  {
+                    name: "Player 2",
+                    score: 0
+                  }
+                ]
+              },
+              {
+                players: [
+                  {
+                    name: "Player 3",
+                    score: 1
+                  },
+                  {
+                    name: "Player 4",
+                    score: 0
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            best: 1,
+            matches: [
+              {
+                players: [
+                  {
+                    name: "Player 1",
+                    score: 0
+                  },
+                  {
+                    name: "Player 3",
+                    score: 0
+                  }
+                ]
+              }
+            ]
+          }
+        ]
       }
     };
   }
 
   render() {
+    const { tournamnet } = this.state;
     // const { id: players } = this.props.match;
 
     return (
       <React.Fragment>
-        <div className="bg-dark overflow-auto">
-          <div className="d-md-flex align-items-stretch justify-content-center min-vh-100">
-            <div className="d-md-flex flex-column justify-content-around align-items-center mr-2">
-              <Match />
-              <Match />
-            </div>
-            <div className="d-md-flex flex-column justify-content-around align-items-center mr-2">
-              <Match />
-            </div>
-            <div className="d-md-flex flex-column justify-content-around align-items-center">
-              <Match />
-            </div>
-            <div className="d-md-flex flex-column justify-content-around align-items-center ml-2">
-              <Match />
-            </div>
-            <div className="d-md-flex flex-column justify-content-around align-items-center mr-2">
-              <Match />
-              <Match />
-            </div>
+        <div className="test">xd</div>
+        <div className="bg-dark overflow-auto flex-fill d-flex">
+          <div className="d-md-flex align-items-stretch justify-content-center flex-fill">
+            {tournamnet.rounds.map((round, round_index) => (
+              <div
+                key={round_index}
+                className="d-md-flex flex-column justify-content-around align-items-center mr-2"
+              >
+                {round.matches.map((match, match_index) => (
+                  <Match key={match_index} data={match} />
+                ))}
+              </div>
+            ))}
           </div>
         </div>
       </React.Fragment>
