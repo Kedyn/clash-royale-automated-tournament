35c9a9738aedca5f31cda3fe38f77ae88d948e97
Kedyn Macedonio <mkedyn@gmail.com>
2019-12-07 11:41:03 -0800

    moved API to services, changed function to promise

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
+import { getPlayerGames } from "services/ClashRoyale";
 
 export default class HomePage extends Component {
+  componentDidMount() {
+    getPlayerGames().then(battles => {
+      console.log(battles);
+    });
+  }
+
   render() {
     return <div></div>;
   }
