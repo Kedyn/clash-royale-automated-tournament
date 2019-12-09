b33f3b7f5538eee634bca40c22ac8ee6cbcbbcf8
Kedyn Macedonio <mkedyn@gmail.com>
2019-12-09 00:26:55 -0800

    fixed redirected after login and registration

--- src/components/layout/NavigationBar.jsx
+++ src/components/layout/NavigationBar.jsx
@@ -30,7 +30,7 @@ class NavigationBar extends Component {
 
   hideLogin(loggedin) {
     if (loggedin) {
-      this.props.history.push("/account");
+      this.props.history.push("/tournaments");
     }
 
     this.setState({ showLogin: false });
@@ -42,7 +42,7 @@ class NavigationBar extends Component {
 
   hideRegister(registered) {
     if (registered) {
-      this.props.history.push("/account");
+      this.props.history.push("/tournaments");
     }
 
     this.setState({ showRegister: false });
